cad6723e88a3b27e42ce9a7cf858481b561a9002
Thepnatee Phojan <thepnatee.p@gmail.com>
2020-12-22 15:04:51 +0700

    Update index.php

--- index.php
+++ index.php
@@ -157,149 +157,180 @@ function sentLineMessage_text($message = array())
             # https://developers.line.biz/flex-simulator/
             # ใช้ในการวาด Flex
             $messages = '
-            {
-            "replyToken":"' . $message['replyToken'] . '",
-            "messages":[
+                                {
+                                "replyToken":"' . $message['replyToken'] . '",
+                                "messages":[
 
-              {
-                "type": "flex",
-                "altText": "Getting Personal Information",
-                "contents": {
-                {
-                    "type": "bubble",
-                    "body": {
-                      "type": "box",
-                      "layout": "vertical",
-                      "contents": [
-                        {
-                          "type": "image",
-                          "url": "https://scdn.line-apps.com/n/channel_devcenter/img/flexsnapshot/clip/clip3.jpg",
-                          "size": "full",
-                          "aspectMode": "cover",
-                          "aspectRatio": "1:1",
-                          "gravity": "center"
-                        },
-                        {
-                          "type": "box",
-                          "layout": "vertical",
-                          "contents": [],
-                          "position": "absolute",
-                          "background": {
-                            "type": "linearGradient",
-                            "angle": "0deg",
-                            "endColor": "#00000000",
-                            "startColor": "#00000099"
-                          },
-                          "width": "100%",
-                          "height": "40%",
-                          "offsetBottom": "0px",
-                          "offsetStart": "0px",
-                          "offsetEnd": "0px"
-                        },
-                        {
-                          "type": "box",
-                          "layout": "horizontal",
-                          "contents": [
-                            {
-                              "type": "box",
-                              "layout": "vertical",
-                              "contents": [
+
                                 {
-                                  "type": "box",
-                                  "layout": "horizontal",
-                                  "contents": [
+                                    "type": "flex",
+                                    "altText": "Demo Flex",
+                                    "contents": 
+                                    
                                     {
-                                      "type": "text",
-                                      "text": "Brown Grand Hotel",
-                                      "size": "xl",
-                                      "color": "#ffffff"
-                                    }
-                                  ]
-                                },
-                                {
-                                  "type": "box",
-                                  "layout": "baseline",
-                                  "contents": [
-                                    {
-                                      "type": "icon",
-                                      "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/review_gold_star_28.png"
-                                    },
-                                    {
-                                      "type": "icon",
-                                      "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/review_gold_star_28.png"
-                                    },
-                                    {
-                                      "type": "icon",
-                                      "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/review_gold_star_28.png"
-                                    },
-                                    {
-                                      "type": "icon",
-                                      "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/review_gold_star_28.png"
-                                    },
-                                    {
-                                      "type": "icon",
-                                      "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/review_gray_star_28.png"
-                                    },
-                                    {
-                                      "type": "text",
-                                      "text": "4.0",
-                                      "color": "#a9a9a9"
-                                    }
-                                  ],
-                                  "spacing": "xs"
-                                },
-                                {
-                                  "type": "box",
-                                  "layout": "horizontal",
-                                  "contents": [
-                                    {
-                                      "type": "box",
-                                      "layout": "baseline",
-                                      "contents": [
-                                        {
-                                          "type": "text",
-                                          "text": "¥62,000",
-                                          "color": "#ffffff",
-                                          "size": "md",
-                                          "flex": 0,
-                                          "align": "end"
-                                        },
-                                        {
-                                          "type": "text",
-                                          "text": "¥82,000",
-                                          "color": "#a9a9a9",
-                                          "decoration": "line-through",
-                                          "size": "sm",
-                                          "align": "end"
+                                      "type": "bubble",
+                                      "hero": {
+                                        "type": "image",
+                                        "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/01_1_cafe.png",
+                                        "size": "full",
+                                        "aspectRatio": "20:13",
+                                        "aspectMode": "cover",
+                                        "action": {
+                                          "type": "uri",
+                                          "label": "Line",
+                                          "uri": "https://linecorp.com/"
                                         }
-                                      ],
-                                      "flex": 0,
-                                      "spacing": "lg"
+                                      },
+                                      "body": {
+                                        "type": "box",
+                                        "layout": "vertical",
+                                        "contents": [
+                                          {
+                                            "type": "text",
+                                            "text": "Brown Cafe",
+                                            "weight": "bold",
+                                            "size": "xl",
+                                            "contents": []
+                                          },
+                                          {
+                                            "type": "box",
+                                            "layout": "baseline",
+                                            "margin": "md",
+                                            "contents": [
+                                              {
+                                                "type": "icon",
+                                                "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/review_gold_star_28.png",
+                                                "size": "sm"
+                                              },
+                                              {
+                                                "type": "icon",
+                                                "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/review_gold_star_28.png",
+                                                "size": "sm"
+                                              },
+                                              {
+                                                "type": "icon",
+                                                "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/review_gold_star_28.png",
+                                                "size": "sm"
+                                              },
+                                              {
+                                                "type": "icon",
+                                                "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/review_gold_star_28.png",
+                                                "size": "sm"
+                                              },
+                                              {
+                                                "type": "icon",
+                                                "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/review_gray_star_28.png",
+                                                "size": "sm"
+                                              },
+                                              {
+                                                "type": "text",
+                                                "text": "4.0",
+                                                "size": "sm",
+                                                "color": "#999999",
+                                                "flex": 0,
+                                                "margin": "md",
+                                                "contents": []
+                                              }
+                                            ]
+                                          },
+                                          {
+                                            "type": "box",
+                                            "layout": "vertical",
+                                            "spacing": "sm",
+                                            "margin": "lg",
+                                            "contents": [
+                                              {
+                                                "type": "box",
+                                                "layout": "baseline",
+                                                "spacing": "sm",
+                                                "contents": [
+                                                  {
+                                                    "type": "text",
+                                                    "text": "Place",
+                                                    "size": "sm",
+                                                    "color": "#AAAAAA",
+                                                    "flex": 1,
+                                                    "contents": []
+                                                  },
+                                                  {
+                                                    "type": "text",
+                                                    "text": "Miraina Tower, 4-1-6 Shinjuku, Tokyo",
+                                                    "size": "sm",
+                                                    "color": "#666666",
+                                                    "flex": 5,
+                                                    "wrap": true,
+                                                    "contents": []
+                                                  }
+                                                ]
+                                              },
+                                              {
+                                                "type": "box",
+                                                "layout": "baseline",
+                                                "spacing": "sm",
+                                                "contents": [
+                                                  {
+                                                    "type": "text",
+                                                    "text": "Time",
+                                                    "size": "sm",
+                                                    "color": "#AAAAAA",
+                                                    "flex": 1,
+                                                    "contents": []
+                                                  },
+                                                  {
+                                                    "type": "text",
+                                                    "text": "10:00 - 23:00",
+                                                    "size": "sm",
+                                                    "color": "#666666",
+                                                    "flex": 5,
+                                                    "wrap": true,
+                                                    "contents": []
+                                                  }
+                                                ]
+                                              }
+                                            ]
+                                          }
+                                        ]
+                                      },
+                                      "footer": {
+                                        "type": "box",
+                                        "layout": "vertical",
+                                        "flex": 0,
+                                        "spacing": "sm",
+                                        "contents": [
+                                          {
+                                            "type": "button",
+                                            "action": {
+                                              "type": "uri",
+                                              "label": "CALL",
+                                              "uri": "https://linecorp.com"
+                                            },
+                                            "height": "sm",
+                                            "style": "link"
+                                          },
+                                          {
+                                            "type": "button",
+                                            "action": {
+                                              "type": "uri",
+                                              "label": "WEBSITE",
+                                              "uri": "https://linecorp.com"
+                                            },
+                                            "height": "sm",
+                                            "style": "link"
+                                          },
+                                          {
+                                            "type": "spacer",
+                                            "size": "sm"
+                                          }
+                                        ]
+                                      }
+                                  
                                     }
-                                  ]
+
+
+
+                                 ]
                                 }
-                              ],
-                              "spacing": "xs"
-                            }
-                          ],
-                          "position": "absolute",
-                          "offsetBottom": "0px",
-                          "offsetStart": "0px",
-                          "offsetEnd": "0px",
-                          "paddingAll": "20px"
-                        }
-                      ],
-                      "paddingAll": "0px"
-                    }
-                  }
-
-                }
-              }
-
-                  
-                  ]
-            }
-  ';
+                      ';
 
 
             $message['post'] = $messages;
